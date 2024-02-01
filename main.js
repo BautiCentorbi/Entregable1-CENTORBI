8ba70d2e08f8a1f791c9ee733e64f0153179d6b9
Bautista Centorbi <bautii.centorbii@gmail.com>
2024-02-01 13:28:47 -0300

    Correcciones Entregable-1

--- main.js
+++ main.js
@@ -1,21 +1,44 @@
-let cantidadJugadores = parseInt(prompt("Ingrese la cantidad de jugadores que desea analizar"));
+let cantidadJugadores
+while (true) {
+    cantidadJugadores = parseInt(prompt("Ingrese la cantidad de jugadores que desee analizar"));
+    if (cantidadJugadores > 0){
+        break;
+    } else {
+        console.log("Has ingresado un valor negativo, por favor intenta de nuevo.")
+    }
+}
+
 let jugadoresTotales = [];
 
 for (let i = 0; i < cantidadJugadores; i++) {
     jugadoresTotales[i] = [prompt("Nombre Jugador " + (i + 1)), 0];
 }
 
-let partidosTotales = parseInt(prompt("Ingrese la cantidad de partidos que desee analizar"));
-let puntosPartido = [];
+let partidosTotales
+while (true) {
+    partidosTotales = parseInt(prompt("Ingrese la cantidad de partidos que desee analizar"));
+    if (partidosTotales > 0){
+        break;
+    } else {
+        console.log("Has ingresado un valor negativo, por favor intenta de nuevo.")
+    }
+} 
 
 // Funcion Promedio
-function promediarEstadistica (estadistica, mensaje){
+function promediarEstadistica (mensaje){
     let estadisticaPartido = []
 
     for (let m = 0; m < partidosTotales; m++) {
         for (let i = 0; i < cantidadJugadores; i++) {
-            estadisticaPartido[m] = estadisticaPartido[m] || []
-            estadisticaPartido[m][i] = parseInt(prompt(`¿Cuántos ${mensaje} hizo ${jugadoresTotales[i][0]} en el partido ${m + 1}?`))
+            while (true){
+                estadisticaPartido[m] = estadisticaPartido[m] || []
+                estadisticaPartido[m][i] = parseInt(prompt(`¿Cuántos ${mensaje} hizo ${jugadoresTotales[i][0]} en el partido ${m + 1}?`))
+                if (estadisticaPartido[m][i] > 0){
+                    break;
+                } else {
+                    console.log("Has ingresado un valor negativo, por favor intenta de nuevo.")
+                }
+            }
         }
     }   
     for (let i = 0; i < cantidadJugadores; i++) {
@@ -36,13 +59,13 @@ while (funcionamiento == true) {
     let decisionUsuario = parseInt(prompt("¿Qué estadística deseas calcular? Selecciona el número correspondiente a cada acción por favor:\n 1. Puntos\n 2. Rebotes\n 3. Asitencias\n 4. Salir"))
     switch (decisionUsuario){
         case 1:
-            promediarEstadistica([], "puntos")
+            promediarEstadistica("puntos")
             break;
         case 2:
-            promediarEstadistica([], "rebotes")
+            promediarEstadistica("rebotes")
             break;
         case 3:
-            promediarEstadistica([], "asistencias")
+            promediarEstadistica("asistencias")
             break;
         case 4:
             funcionamiento = false
